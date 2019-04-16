2ed7c2c90c267469a4008bd8a87c1939ab7282c0
xibeijia <xibeijia@xibeijia-PC>
2019-04-16 22:27:14 +0800

    xiugai111

--- src/default1/HelloWorld.java
+++ src/default1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world !!!!");
 		System.out.println("java forever !!!");
+		System.out.println("自己本地的项目，修改一下上传到git");
 	}
 
 }
