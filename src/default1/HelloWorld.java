fe7d66c8338de980b9afdf6277b741787a69ee1d
xibeijia <xibeijia@xibeijia-PC>
2019-04-16 22:04:14 +0800

    test

---
+++ src/default1/HelloWorld.java
@@ -0,0 +1,10 @@
+package default1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world !!!!");
+		System.out.println("java forever !!!");
+	}
+
+}
