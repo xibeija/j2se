050af8ee55a6bbd15385be44b9fea2ecd82d9949
xibeijia <xibeijia@xibeijia-PC>
2019-04-17 23:44:52 +0800

    bianliang

--- src/default1/HelloWorld.java
+++ src/default1/HelloWorld.java
@@ -7,6 +7,20 @@ public class HelloWorld {
 		System.out.println("java forever !!!");
 		System.out.println("自己本地的项目，修改一下上传到git");
 		System.out.println("再来一次");
+		int oxVal = 032;
+		System.out.println(oxVal);
+		char c = 'A';
+		short s = 80;
+		c = (char)s;
+		System.out.println(c);
+		byte b = 5;
+		int i1 = 10;
+		int i2 = 300;
+		b = (byte)i1;
+		System.out.println(b);
+		b = (byte)i2;
+		System.out.println(b);
+		System.out.println(Integer.toBinaryString(i2));
 	}
 
 }
