048c6b395bf8faed0d5c4aae26214e38299103ce
yousheng_jia <yousheng_jia@ca8108620>
2019-04-20 18:35:29 +0800

    Mac peizhi

--- src/default1/HelloWorld.java
+++ src/default1/HelloWorld.java
@@ -30,6 +30,7 @@ public class HelloWorld {
 		System.out.println("----------------------------");
 		HelloWorld kk = new HelloWorld();
 		kk.method2(33333);*/
+		System.out.println("哈哈，Mac配置成功啦！！！！！");
 		System.out.println("++++++++++++++++++++++++++++++++++");
 		Scanner s = new Scanner(System.in);
 		System.out.println(s.nextLine());
