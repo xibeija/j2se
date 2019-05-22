bfedeb2fd6b11fd8f74508547f8a0d40999a0ad3
xibeijia <xibeijia@xibeijia-PC>
2019-05-23 00:30:01 +0800

    hutubill

---
+++ DB/android.sql
@@ -0,0 +1,23 @@
+# Host: 127.0.0.1  (Version: 5.5.15)
+# Date: 2019-05-23 00:28:33
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES gb2312 */;
+
+#
+# Structure for table "dictionary"
+#
+
+DROP TABLE IF EXISTS `dictionary`;
+CREATE TABLE `dictionary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `receive` varchar(100) DEFAULT NULL,
+  `response` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "dictionary"
+#
+
+INSERT INTO `dictionary` VALUES (1,'你好','好你妹！'),(2,'你叫什么','你想泡我啊？'),(3,'你叫什么','同志，不约'),(4,'打你哦','来啊，来打我啊，不打有点挫');
---
+++ DB/hutubill.sql
@@ -0,0 +1,59 @@
+# Host: 127.0.0.1  (Version: 5.5.15)
+# Date: 2019-05-23 00:27:44
+# Generator: MySQL-Front 5.3  (Build 4.269)
+
+/*!40101 SET NAMES gb2312 */;
+
+#
+# Structure for table "category"
+#
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "category"
+#
+
+
+#
+# Structure for table "config"
+#
+
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key_` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "config"
+#
+
+
+#
+# Structure for table "record"
+#
+
+DROP TABLE IF EXISTS `record`;
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `spend` int(11) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_record_category` (`cid`),
+  CONSTRAINT `fk_record_category` FOREIGN KEY (`cid`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "record"
+#
+
